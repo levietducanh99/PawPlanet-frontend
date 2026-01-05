0004527b889c00c1b1505e2221cb338d4c71289c
hoan99111 <hoan99111@gmail.com>
2026-01-05 16:44:51 +0700

    feat: integrate authentication service with login and registration flows

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useFetch } from './useFetch';
 export { useMediaUpload } from './useMediaUpload';
+export { useLogin, useRegister, useAuth } from './useAuth';
 export {
   usePetProfile,
   usePetTimeline,
